5e17eacf63f8fc603ade04fa315e7e4918b75f7b
Artem Larin <artem_larin1@epam.com>
2022-01-24 17:37:48 +0200

    Updated to PEP 8 coding standard

--- HW1.py
+++ HW1.py
@@ -1,37 +1,37 @@
-#imports for values generation
+# imports for values generation
 from random import seed
 from random import randint
 
-#ensures that generated values will be the same with every run
+# ensures that generated values will be the same with every run
 seed(1)
 
-#creating the list of 100 generated int values from 1 to 1000
-sample = [randint(1,1000) for i in range(99)] 
-#print(sample)
+# creating the list of 100 generated int values from 1 to 1000
+sample = [randint(1, 1000) for i in range(99)]
+# print(sample)
 
-#bubble sort
+# bubble sort
 for i in range(len(sample)):
-    for j in range(i+1, len(sample)):
-        if sample[i]>sample[j]:
+    for j in range(i + 1, len(sample)):
+        if sample[i] > sample[j]:
             sample[i], sample[j] = sample[j], sample[i]
-#print(sample)
+# print(sample)
 
-#creating 2 new empty lists for even and odd numbers
-even=[]
-odd=[]
+# creating 2 new empty lists for even and odd numbers
+even = []
+odd = []
 
-#appending values to even or odd list
+# appending values to even or odd list
 for i in range(len(sample)):
-    if sample[i]%2==0:
+    if sample[i] % 2 == 0:
         even.append(sample[i])
     else:
         odd.append(sample[i])
-#print(even)
-#print(odd)
+# print(even)
+# print(odd)
 
-#calculating and printing avg values for both lists
-even_avg=sum(even)/len(even)
+# calculating and printing avg values for both lists
+even_avg = sum(even) / len(even)
 print('Avg of the even numbers: ', round(even_avg, 3))
 
-odd_avg=sum(odd)/len(odd)
+odd_avg = sum(odd) / len(odd)
 print('Avg of the odd numbers: ', round(odd_avg, 3))
