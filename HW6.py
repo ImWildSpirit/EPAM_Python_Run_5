c05d573f1238c17f22b2674ffe2bc9509ee7c576
Artem Larin <artem_larin1@epam.com>
2022-02-19 00:26:19 +0200

    Homework #6 v1.2 - Minor PEP8 changes

--- HW6.py
+++ HW6.py
@@ -32,7 +32,7 @@ class ArticleFromFile(PrivateAd, Review, News):
             self.unpublished += 1
 
     def publish(self):
-        if self.from_file == None:
+        if self.from_file is None:
             print('File for reading was not specified')
             exit()
         else:
@@ -43,26 +43,26 @@ class ArticleFromFile(PrivateAd, Review, News):
             for i in range(len(tmp)):
                 if findall(r'(\*N\*)', tmp[i]):
                     elem = tmp[i].splitlines()
-                    
-                    for i in range(len(elem)):
-                        if elem[i] == '*N*':
-                            if elem[i+1] != '' or not findall(r'<<\D*>>', elem[i+1]):
-                                elem[i] = ''
-                            
+
+                    for j in range(len(elem)):
+                        if elem[j] == '*N*':
+                            if elem[j + 1] != '' or not findall(r'<<\D*>>', elem[j + 1]):
+                                elem[j] = ''
+
                             else:
                                 self.validator = False
                                 self.unpublished += 1
-                            
-                        elif findall(r'<<\D*>>', elem[i]):
-                            _city = sub('<<', '', elem[i])
+
+                        elif findall(r'<<\D*>>', elem[j]):
+                            _city = sub('<<', '', elem[j])
                             self.city = sub('>>', '', _city).strip()
 
                             if self.city != '':
-                                elem[i] = sub(self.city, '', self.city)
+                                elem[j] = sub(self.city, '', self.city)
                             else:
                                 self.validator = False
-                                self.unpublished +=1
-                        
+                                self.unpublished += 1
+
                     _text = text_normalizer('\n'.join(elem))
                     if _text != '':
                         self.text = _text
@@ -78,40 +78,41 @@ class ArticleFromFile(PrivateAd, Review, News):
                                 file.write(f'News feed:\n\n\n-----News:-----\n{self.text}\n{self.city}, {self.time}')
 
                         else:
-                            if stat(self.file_name).st_size != 0: 
+                            if stat(self.file_name).st_size != 0:
                                 with open(self.file_name, 'a') as file:
                                     file.write(f'\n\n\n-----News:-----\n{self.text}\n{self.city}, {self.time}')
 
                             else:
                                 with open(self.file_name, 'w') as file:
-                                    file.write(f'News feed:\n\n\n-----News:-----\n{self.text}\n{self.city}, {self.time}')
+                                    file.write(
+                                        f'News feed:\n\n\n-----News:-----\n{self.text}\n{self.city}, {self.time}')
                     else:
                         self.validator = True
                         continue
-                
+
                 elif findall(r'(\*A\*)', tmp[i]):
                     elem = tmp[i].splitlines()
 
-                    for i in range(len(elem)):
-                        if findall(r'<<\d\d/\d\d/\d{4}>>', elem[i]):
-                            _exp = sub('<<', '', elem[i])
+                    for j in range(len(elem)):
+                        if findall(r'<<\d\d/\d\d/\d{4}>>', elem[j]):
+                            _exp = sub('<<', '', elem[j])
                             _exp = sub('>>', '', _exp).strip()
 
                             self.exp_date = datetime.strptime(_exp, '%d/%m/%Y').date()
-                            self.expire_count = self.get_days_till_expire(custom_date = self.exp_date)
+                            self.expire_count = self.get_days_till_expire(custom_date=self.exp_date)
 
                             if self.validator:
-                                elem[i] = sub(datetime.strftime(self.exp_date, '%d/%m/%Y'), '', _exp)
+                                elem[j] = sub(datetime.strftime(self.exp_date, '%d/%m/%Y'), '', _exp)
 
-                        elif elem[i] == '*A*':
-                            elem[i] = elem[i].replace('*A*', '').strip()
-                        
-                        elif findall(r'<<>>', elem[i]):
+                        elif elem[j] == '*A*':
+                            elem[j] = elem[j].replace('*A*', '').strip()
+
+                        elif findall(r'<<>>', elem[j]):
                             self.validator = False
                             self.unpublished += 1
-                        
+
                         self.text = text_normalizer('\n'.join(elem))
-                        
+
                         if self.text == '':
                             self.validator = False
                             self.unpublished += 1
@@ -119,65 +120,68 @@ class ArticleFromFile(PrivateAd, Review, News):
                     if self.validator:
                         if not path.exists(self.file_name):
                             with open(self.file_name, 'w+') as file:
-                                file.write(f'News feed:\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date}, {self.expire_count}')
+                                file.write(
+                                    f'News feed:\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date}, {self.expire_count}')
 
                         else:
                             if stat(self.file_name).st_size != 0:
                                 with open(self.file_name, 'a') as file:
-                                    file.write(f'\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date.strftime("%d/%m/%Y")}, {self.expire_count}')
+                                    file.write(
+                                        f'\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date.strftime("%d/%m/%Y")}, {self.expire_count}')
                             else:
                                 with open(self.file_name, 'w') as file:
-                                    file.write(f'News feed:\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date.strftime("%d/%m/%Y")}, {self.expire_count}')
+                                    file.write(
+                                        f'News feed:\n\n\n-----Private ad:-----\n{self.text}\nActual until: {self.exp_date.strftime("%d/%m/%Y")}, {self.expire_count}')
                     else:
                         self.validator = True
                         continue
-                
+
                 elif findall(r'\*R\*', tmp[i]):
                     elem = tmp[i].splitlines()
 
-                    for i in range(len(elem)):
-                        if elem[i] == '*R*':
-                            if elem[i+1] != '':
-                                self.title = text_normalizer(elem[i+1].strip())
-                            
+                    for j in range(len(elem)):
+                        if elem[j] == '*R*':
+                            if elem[j + 1] != '':
+                                self.title = text_normalizer(elem[j + 1].strip())
+
                             else:
                                 self.validator = False
                                 self.unpublished += 1
 
-                            elem[i+1] = ''
-                            elem[i] = ''
+                            elem[j + 1] = ''
+                            elem[j] = ''
 
-                        elif findall(r'<<\D*>>', elem[i]):
-                            _author = sub('<<', '', elem[i])
+                        elif findall(r'<<\D*>>', elem[j]):
+                            _author = sub('<<', '', elem[j])
                             self.author = sub('>>', '', _author).strip()
-                            
+
                             if self.author != '':
-                                elem[i] = sub(self.author, '', self.author)
-                                
-                            else:
-                                self.validator = False
-                                self.unpublished += 1
-                        
-                        elif findall(r'<<\d\d?>>', elem[i]):
-                            _rate = sub('<<', '', elem[i])
-                            self.rate = sub('>>', '', _rate).strip()
-                            
-                            if self.rate != '' or 1 <= int(self.rate) <= 10:
-                                elem[i] = sub(self.rate, '', self.rate)
-                                
+                                elem[j] = sub(self.author, '', self.author)
+
                             else:
                                 self.validator = False
                                 self.unpublished += 1
 
-                        elif findall(r'<<>>', elem[i]):
+                        elif findall(r'<<\d\d?>>', elem[j]):
+                            _rate = sub('<<', '', elem[j])
+                            self.rate = sub('>>', '', _rate).strip()
+
+                            if self.rate != '' or 1 <= int(self.rate) <= 10:
+                                elem[j] = sub(self.rate, '', self.rate)
+
+                            else:
+                                self.validator = False
+                                self.unpublished += 1
+
+                        elif findall(r'<<>>', elem[j]):
                             self.validator = False
                             self.unpublished += 1
-                    
+
                     _text = text_normalizer('\n'.join(elem))
-                    
+
                     if _text != '':
                         self.text = _text
-                    
+
                     else:
                         self.validator = False
                         self.unpublished += 1
@@ -187,21 +191,26 @@ class ArticleFromFile(PrivateAd, Review, News):
                     if self.validator:
                         if not path.exists(self.file_name):
                             with open(self.file_name, 'w+') as file:
-                                file.write(f'News feed:\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
+                                file.write(
+                                    f'News feed:\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
 
                         else:
                             if stat(self.file_name).st_size != 0:
                                 with open(self.file_name, 'a') as file:
-                                    file.write(f'\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
+                                    file.write(
+                                        f'\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
                             else:
                                 with open(self.file_name, 'w') as file:
-                                    file.write(f'News feed:\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
+                                    file.write(
+                                        f'News feed:\n\n\n-----Review:-----\n{self.title}\n{self.text}\nFinal score: {self.rate}/10,\n{self.author}, {self.time}')
                     else:
                         self.validator = True
                         continue
 
-            remove(self.from_file)                           
-            print(self.unpublished, 'publications skipped due to errors in the source file.') if self.unpublished > 0 else print('Success.')
+            remove(self.from_file)
+            print(self.unpublished,
+                  'publications skipped due to errors in the source file.') if self.unpublished > 0 else print(
+                'Success.')
 
 
 def main(from_file=None, file_name='Publication.txt'):
@@ -225,12 +234,12 @@ def main(from_file=None, file_name='Publication.txt'):
             Review(file_name)
 
         elif article == '4':
-                if from_file == None:
-                    print('File for reading was not specified. Use -s or --source argument to specify path')
-                    exit()
-                else:
-                    print('------------Uploading------------')
-                    ArticleFromFile(from_file, file_name)
+            if from_file is None:
+                print('File for reading was not specified. Use -s or --source argument to specify path')
+                exit()
+            else:
+                print('------------Uploading------------')
+                ArticleFromFile(from_file, file_name)
 
         else:
             print('Incorrect article type\n')
@@ -241,10 +250,11 @@ def main(from_file=None, file_name='Publication.txt'):
         print('\nTerminated.')
         exit()
 
-    #Additional article confirmation
+    # Additional article confirmation
     if article == '4':
         try:
-            confirm = input('Do you want to upload another file? (Input y to confirm, input any other button to exit)\n')
+            confirm = input(
+                'Do you want to upload another file? (Input y to confirm, input any other button to exit)\n')
 
             if confirm.lower().strip() == 'y':
                 try:
@@ -256,20 +266,21 @@ def main(from_file=None, file_name='Publication.txt'):
                 except KeyboardInterrupt:
                     print('\nTerminated')
                     exit()
-            
+
                 main(from_file, file_name)
 
             else:
                 exit()
 
-        #KeyboardInterrupt error handling
+        # KeyboardInterrupt error handling
         except KeyboardInterrupt:
             print('\nTerminated.')
             exit()
-    
+
     else:
         try:
-            confirm = input('Do you want to add another article? (Input y to add another article, input any other button to exit)\n')
+            confirm = input(
+                'Do you want to add another article? (Input y to add another article, input any other button to exit)\n')
 
             if confirm.lower().strip() == 'y':
                 main(file_name)
@@ -282,19 +293,21 @@ def main(from_file=None, file_name='Publication.txt'):
             print('\nTerminated.')
             exit()
 
+
 # Script entry point. It will run only if not imported to another module.
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--source', type = str, help = 'Path to source file')
-    parser.add_argument('-d', '--dest', type = str, help = 'Path to destination file')
+    parser.add_argument('-s', '--source', type=str, help='Path to source file')
+    parser.add_argument('-d', '--dest', type=str, help='Path to destination file')
     args = parser.parse_args()
 
     from_file = args.source
     file_name = args.dest
 
-    if from_file == None and file_name == None:
-        print('----Script without arguments will run with testing defaults.----\nSource file - Source.txt,\nDestination file - Publication.txt\nUse -h or --help to show available arguments.\n\n')
-        main(from_file = 'Source.txt', file_name = 'Publication.txt')
+    if from_file is None and file_name is None:
+        print(
+            '----Script without arguments will run with testing defaults.----\nSource file - Source.txt,\nDestination file - Publication.txt\nUse -h or --help to show available arguments.\n\n')
+        main(from_file='Source.txt', file_name='Publication.txt')
     else:
         main(from_file)
